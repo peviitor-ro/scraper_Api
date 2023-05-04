eba6a16a656cd5a5ae2869c86700253ceb6372a0
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-04 17:47:13 +0300

    create scrapers add function for api

--- scraper_Api/Api/urls.py
+++ scraper_Api/Api/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-from rest_framework import routers
 from . import views
 import os
 
 urlpatterns = [
-    
+    path('add/' , views.AddView.as_view()),
 ]
 
 subfolders = [f.path for f in os.scandir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'scrapers')) if f.is_dir() ]
 
 for folder in subfolders:
-    urlpatterns.append(path(folder.split('/')[-1]+'/', views.ApiView.as_view()))
+    urlpatterns.append(path(folder.split('/')[-1]+'/', views.ScraperView.as_view()))
 
--- scraper_Api/Api/views.py
+++ scraper_Api/Api/views.py
@@ -1,13 +1,13 @@
 from rest_framework.response import Response  
-from rest_framework import viewsets
-from rest_framework import permissions
 from rest_framework.views import APIView  
 import os
 import subprocess
+from django.urls import clear_url_caches
 
 
 
-class ApiView(APIView):
+
+class ScraperView(APIView):
     
     """
     API endpoint that allows users to be viewed or edited.
@@ -40,23 +40,50 @@ class ApiView(APIView):
 
         if file != None:
             command = self.extensions[file.split('.')[-1]]
-            subproces = subprocess.run([command , os.path.join(scrapersFolder, file)], capture_output=True, text=True)
+            process = subprocess.run([command , os.path.join(scrapersFolder, file)], capture_output=True)
             log = dict()
-            if subproces.returncode == 0:
-                log['succes'] = subproces.stdout
+            if process.returncode == 0:
+                log['succes'] = process.stdout
             else:
-                log['error'] = subproces.stderr
+                log['error'] = process.stderr
 
             return Response(log)
         
         if update != None:
-            subproces = subprocess.run(['git', 'pull', 'origin', 'main'], capture_output=True, text=True)
+            process = subprocess.run(['git', 'pull', 'origin', 'main'], capture_output=True, text=True)
             log = dict()
-            if subproces.returncode == 0:
-                log['succes'] = subproces.stdout
+            if process.returncode == 0:
+                log['succes'] = process.stdout
             else:
-                log['error'] = subproces.stderr
+                log['error'] = process.stderr
             return Response(log)
         
         return Response('error')
+    
+class AddView(APIView):
+    
+    def post (self, request, format=None):
+        url = request.data.get('url')
+        path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'scrapers')
+        
+        if url != None:
+            process = subprocess.Popen(['git', 'clone', url], cwd=path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+
+            log = dict()
+            if process.returncode == 0:
+                log['succes'] = stdout.decode('utf-8') + 'succes'
+            else:
+                log['error'] = stderr.decode('utf-8')
+
+            return Response(log)
+        
+        clear_url_caches()
+        
+        return Response('error')
+    
+class RemoveView(AddView):
+    def get (self, request):
+        folders = [f.path for f in os.scandir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'scrapers')) if f.is_dir() ]
+        return Response(folders)
         

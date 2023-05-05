bc645c55cfb6802d5525c4ae34468fedd716bb4a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-05 16:31:56 +0300

    adding static files root

--- scraper_Api/scraper_Api/settings.py
+++ scraper_Api/scraper_Api/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join('root')
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

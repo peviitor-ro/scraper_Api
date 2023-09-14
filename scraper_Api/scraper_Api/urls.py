f2ba707f209d042978ae61d181cb42b67e344080
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-09-14 17:00:08 +0300

    adding method to get data from peviitor

--- scraper_Api/scraper_Api/urls.py
+++ scraper_Api/scraper_Api/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from Api.views import PeviitorData
 
 urlpatterns = [
+    path('', PeviitorData.as_view()),
     path('admin/', admin.site.urls),
     path('scraper/', include('Api.urls')),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}, name='static'),

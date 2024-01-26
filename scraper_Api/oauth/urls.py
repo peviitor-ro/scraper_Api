e8d66ed4f7565f9555c85f1c7e9355f663cf2614
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-01-26 13:22:46 +0200

    Remove OAuth related files and code

--- scraper_Api/oauth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views 
-from .views import LoginView
-
-urlpatterns = [
-    path('homepage/', LoginView.as_view(template_name='dist/index.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]

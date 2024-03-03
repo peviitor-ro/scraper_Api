408d8a32bc245a864e3d782ddd0215dc56bdaabf
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-03-03 22:17:04 +0200

    Remove unused code files

--- scraper_Api/jobs/constants.py
+++
@@ -1,8 +0,0 @@
-COMPANY_SORT_OPTIONS = {
-    "name_asc": "company",
-    "name_desc": "-company",
-    "job_count_asc": "job_count",
-    "job_count_desc": "-job_count",
-    "last_update_asc": "last_update",
-    "last_update_desc": "-last_update",
-}

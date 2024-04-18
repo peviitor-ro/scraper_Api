b35216718e46293c515227bf4adf56cf742e8130
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-04-19 00:17:16 +0300

    Refactor CityViewSet search functionality to only search by city name in views.py

--- scraper_Api/orase/views.py
+++ scraper_Api/orase/views.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.db.models import Q
 from .serializer import CitySerializer
 
 from .models import City
 from utils.pagination import CustomPagination
+
 
 @permission_classes([AllowAny])
 class CityViewSet(APIView):
@@ -17,13 +17,11 @@ class CityViewSet(APIView):
         search = request.GET.get('search') or ''
 
         queryset = City.objects.filter(
-            Q(name__icontains=search) | Q(county__name__icontains=search)
+            Q(name__icontains=search)
         )
 
         paginator = self.pagination_class()
         result_page = paginator.paginate_queryset(queryset, request)
         serializer = self.serializer_class(result_page, many=True)
 
-        
-        
         return paginator.get_paginated_response(serializer.data)

75a44b01468af50d182f64ee36598365089409cd
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-06-12 17:27:15 +0300

    chore: Update admin page for CustomUser model
    This commit updates the admin page for the CustomUser model in the `users` app. It adds various features and configurations to enhance the usability and functionality of the admin page. These include:
    - A search bar for searching users by email
    - Displaying fields like email, is_active, is_staff, is_superuser, last_login, and date_joined in the list view
    - Filtering users by is_active, is_staff, and is_superuser
    - Sorting users by email
    - Making last_login and date_joined read-only fields
    - Excluding the password field from the admin page
    - Making is_active, is_staff, and is_superuser fields editable in the list view
    - Defining fieldsets for organizing the fields in the detail view
    - Overriding the `get_queryset` method to filter out superusers
    
    These changes improve the user management experience for administrators in the Django admin interface.

--- scraper_Api/users/admin.py
+++ scraper_Api/users/admin.py
@@ -1,4 +1,34 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    
+    # create a search bar for the admin page
+    search_fields = ["email"]
+    # create a list of fields to display
+    list_display = ["email", "is_active", "is_staff", "is_superuser", "last_login", "date_joined"]
+    # create a list of fields to filter
+    list_filter = ["is_active", "is_staff", "is_superuser"]
+    # create a list of fields to sort by
+    ordering = ["email"]
+    # create a list of fields to be read only
+    readonly_fields = ["last_login", "date_joined"]
+    # create a list of fields to be excluded
+    exclude = ["password"]
+    # create a list of fields to be editable
+    list_editable = ["is_active", "is_staff", "is_superuser"]
+    # create a list of fields to be displayed in the detail view
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+        return qs.filter(is_superuser=False)

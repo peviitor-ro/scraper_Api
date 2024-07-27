f9c85db3f7467bc065c2ad083a4e5253ecc4f9f9
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-07-28 00:44:35 +0300

    feat: Increase maximum length of job_link field in Job model to 300 characters

--- scraper_Api/jobs/models.py
+++ scraper_Api/jobs/models.py
@@ -18,7 +18,7 @@ class Job(models.Model):
     country = models.TextField()
     city = models.TextField(blank=True)
     county = models.TextField(blank=True)
-    job_link = models.CharField(max_length=200)
+    job_link = models.CharField(max_length=300)
     job_title = models.TextField()
     remote = models.CharField(max_length=50, blank=True)
     edited = models.BooleanField(default=False)

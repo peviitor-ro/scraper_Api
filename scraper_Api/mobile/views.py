51e8ba47017982486a3a29ef998f13de59264af3
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-09-08 02:43:35 +0300

    feat: Update GetJobView to support filtering by cities, counties, and remote
    The code changes in this commit modify the GetJobView class in the mobile/views.py file. The GetJobView class is responsible for retrieving job data based on various filters. This commit adds support for filtering jobs by cities, counties, and remote status.
    
    The search query parameter is used to filter jobs by job title. The cities and counties query parameters are used to filter jobs by specific cities and counties. The remote query parameter is used to filter jobs that can be done remotely.
    
    These changes enhance the functionality of the GetJobView by allowing users to search for jobs in specific locations and filter jobs that can be done remotely.

--- scraper_Api/mobile/views.py
+++ scraper_Api/mobile/views.py
@@ -5,32 +5,50 @@ from utils.pagination import CustomPagination
 from .serializer import JobSerializer
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
+import json
 
 from jobs.models import Job
+
 
 @permission_classes([AllowAny])
 class GetJobView(APIView):
     serializer_class = JobSerializer
     pagination_class = CustomPagination
+
     def get(self, request):
         search_query = request.GET.get("search") or None
+        cities = request.GET.get("cities") or None
+        counties = request.GET.get("counties") or None
+        remote = request.GET.get("remote") or None
 
-        if not search_query:
-            queryset = Job.objects.all().filter(published=True)
-        else:
-            queryset = Job.objects.filter(
-                Q(job_title__icontains=search_query)
-            ).filter(published=True)
+        filter_search = Q(published=True)
+
+        if search_query:
+            filter_search &= Q(job_title__icontains=search_query)
+
+        if cities and counties:
+            cities_lst = cities.split(",")
+            counties_lst = counties.split(",")
+            
+            for city, county in zip(cities_lst, counties_lst):
+                filter_search &= Q(city__icontains=city) & Q(county__icontains=county)
+
+            filter_search |= Q(city__in=cities_lst) & Q(county__in=counties_lst)
+
+        if remote:
+            remote_lst = remote.split(",")
+            filter_search &= Q(remote__in=remote_lst)
+
+        queryset = Job.objects.filter(filter_search)
 
         paginator = self.pagination_class()
         result_page = paginator.paginate_queryset(queryset, request)
         serializer = self.serializer_class(result_page, many=True)
-
         return paginator.get_paginated_response(serializer.data)
-    
+
+
 @permission_classes([AllowAny])
 class GetTotalJobs(APIView):
     def get(self, _):
         total_jobs = Job.objects.filter(published=True).count()
         return Response({"total": total_jobs})
-

05f4338ea24f8e0d08294e651166ca33921be8d8
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2025-07-17 21:50:36 +0300

    feat: Add custom user creation form and update admin configuration for CustomUser

--- scraper_Api/users/admin.py
+++ scraper_Api/users/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    
-    # create a search bar for the admin page
+    add_form = CustomUserCreationForm
+    form = CustomUserCreationForm
+
     search_fields = ["email", "company__company"]
-    # create a list of fields to display
-    list_display = ["email", "is_active", "is_staff", "is_superuser", "last_login", "date_joined"]
-    # create a list of fields to filter
+    list_display = ["email", "is_active", "is_staff",
+                    "is_superuser", "last_login", "date_joined"]
     list_filter = ["is_active", "is_staff", "is_superuser"]
-    # create a list of fields to sort by
     ordering = ["email"]
-    # create a list of fields to be read only
     readonly_fields = ["last_login", "date_joined"]
-    # create a list of fields to be editable
     list_editable = ["is_active", "is_staff", "is_superuser"]
+
+    # Afișează câmpurile în formularul admin
+    fields = ["email", "company", "scraper", "is_active",
+              "is_staff", "is_superuser", "last_login", "date_joined"]

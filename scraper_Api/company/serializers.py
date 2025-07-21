ce78456646da41d2f0bad144fd6e15c79e8cd486
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2025-07-21 23:28:22 +0300

    feat: Remove debug print statement from CompanySerializer create method

--- scraper_Api/company/serializers.py
+++ scraper_Api/company/serializers.py
@@ -25,9 +25,7 @@ class CompanySerializer(serializers.ModelSerializer):
                   'description', 'logo', 'source','jobsCount', 'published_jobs', 'have_access', 'source_name', 'source_logo']
 
     def create(self, validated_data):
-        print(validated_data)
         instance, create = Company.objects.get_or_create(**validated_data)
-
         if create:
             superusers = CustomUser.objects.filter(is_superuser=True)
             for user in superusers:
